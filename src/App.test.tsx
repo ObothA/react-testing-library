7fe0a1dc811697863916e9d011922e79372c62a5
obotha <khyper234@gmail.com>
2021-10-18 12:04:30 +0300

    Write tests for clicking the button.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('Button has correct initial color.', () => {
   render(<App />);
   const colorButton = screen.getByRole('button', { name: 'Change to blue' });
-  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
-});
 
-test('Button turns blue when clicked.', () => {});
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+  expect(colorButton.textContent).toBe('Change to red');
+});

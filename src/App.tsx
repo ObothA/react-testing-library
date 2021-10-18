c42cb3b2d625386d492c9c01799c0ab8b5b07ac2
obotha <khyper234@gmail.com>
2021-10-18 12:15:18 +0300

    Code on click test.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import logo from './logo.svg';
 
 import './App.css';
 
 function App() {
+  const [buttonColor, setButtonColor] = useState('red');
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+
   return (
     <div>
       <button
         style={{
-          backgroundColor: 'red',
+          backgroundColor: buttonColor,
         }}
+        onClick={() => setButtonColor(newButtonColor)}
       >
-        Change to blue
+        Change to {newButtonColor}
       </button>
     </div>
   );
